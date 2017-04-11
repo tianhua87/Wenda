82e2a62ec120b6ef77648c6fe44f5c40b928bf62
tianhua87 <787156584@qq.com>
2017-04-11 14:27:50 +0800

    LoginTicket  Model

---
+++ src/main/java/com/wen/wenda/model/LoginTicket.java
@@ -0,0 +1,56 @@
+package com.wen.wenda.model;
+
+import java.util.Date;
+
+/**
+ * Created by wen on 2017/4/11.
+ */
+public class LoginTicket {
+    private int id;
+    private int user_id;
+    private Date expired;
+    private int status;
+    private String ticket;
+
+    public LoginTicket(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public Date getExpired() {
+        return expired;
+    }
+
+    public void setExpired(Date expired) {
+        this.expired = expired;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+}
